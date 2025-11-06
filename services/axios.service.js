8af744323339913db26246834af46583cde7e00d
dha2608 <dha260803@gmail.com>
2025-11-06 12:29:35 +0700

    ver 2

--- services/axios.service.js
+++ services/axios.service.js
@@ -1,11 +1,7 @@
 const axios = require('axios');
 
-// API Backend đang chạy trên port 3000
 const API_URL = 'http://localhost:3000';
 
-// Tạo một instance của Axios để dùng chung
-// (Chúng ta không dùng HTTPS như ví dụ demo 
-// vì backend A1 đang là HTTP)
 const apiService = axios.create({
     baseURL: API_URL,
     timeout: 5000,
